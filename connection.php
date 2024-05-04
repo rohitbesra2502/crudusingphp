1c347ee0bdecfcbcb3c6767db5e5222b8a13e0ae
Rohit Besra <77182628+rohitbesra2502@users.noreply.github.com>
2024-05-04 10:20:08 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+    $connection = mysqli_connect("localhost" , "root" , "","crud_operation");
+
+    if(!$connection){
+        die("connection failed");
+    }
+?>
---
+++ delete_page_1.php
@@ -0,0 +1,19 @@
+<?php include('connection.php')?>
+<?php 
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        
+        $query = "delete from `students` where `id` = '$id'";
+
+        $result = mysqli_query($connection , $query);
+
+
+        if(!$result){
+            die("not deleted".mysqli_error());
+        }else{
+            header("location:index.php?delete_msg = Deleted Data");
+        }
+        
+        
+    }
+?>
